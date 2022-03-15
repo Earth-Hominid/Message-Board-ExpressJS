0fec3672dee83b302d12668ee6a6c5c62fb9a43e
C <earthhominid@gmail.com>
2022-03-15 17:03:55 -0300

    jade views removed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "This just in, the FED has updated it's inflation forecast.",
+    user: 'Financial Times',
+    added: new Date(),
+  },
+  {
+    text: "Breaking news, the International Monetary Fund has released it's Q3 forecast.",
+    user: 'The IMF',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express Yes!' });
+  res.render('index', { title: 'Message Board App', messages: messages });
 });
 
 module.exports = router;
