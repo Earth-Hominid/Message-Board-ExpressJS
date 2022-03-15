ba30261b73f78055ec511992df43f19b7945819b
C <earthhominid@gmail.com>
2022-03-15 20:52:48 -0300

    added styling

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ A message board web app created with Express.
 ## Description
 
 The purpose of this project is to create interactive web app using Express.
+
+I have used EJS for the template viewing engine.
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const uuid = require('uuid');
 
 const messages = [
   {
@@ -17,6 +18,21 @@ const messages = [
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Message Board App', messages: messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', (req, res, net) => {
+  const newMessage = {
+    id: uuid.v4(),
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  };
+  messages.push(newMessage);
+  res.redirect('/');
 });
 
 module.exports = router;
